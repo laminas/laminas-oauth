57dda8c1c7a5eb0ee4d48794aec06c1e0bb8d5c0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:09:06 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/TestAsset/HTTPClient39745.php
+++ test/TestAsset/HTTPClient39745.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-oauth for the canonical source repository
- * @copyright https://github.com/laminas/laminas-oauth/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-oauth/blob/master/LICENSE.md New BSD License
- */
-
 namespace LaminasTest\OAuth\TestAsset;
 
 use Laminas\Http\Client;
